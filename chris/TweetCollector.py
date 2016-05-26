0764fc5b0375ac59bb526d1da6ee82b12d832936
Chris Spalding <cspalding93@gmail.com>
2016-05-25 20:33:32 -0700

    small updates to tweetcollector and testing changing .git/info/excludes

--- chris/TweetCollector.py
+++ chris/TweetCollector.py
@@ -1,9 +1,9 @@
 #The line below imports Spark so that we can use its functionality
 from pyspark import SparkContext
+from keys import *
 """
 
 """
-
 class TweetCollector:
     """
     A class for collecting tweets. We use pyspark to create an RDD of tweets and        look at some attributes of the data, such as the most used hashtag or the most verbose tweeters from the tweets collected.
@@ -17,3 +17,8 @@ class TweetCollector:
         self.sc = SparkContext('local','pyspark')
 
     def twitterConnect():
+        pass
+
+if __name__ == "__main__":
+    print(keys.consumer_key)
+    print(keys.consumer_secret)
