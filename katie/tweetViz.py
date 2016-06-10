37e38c6d0521e6ace822986d30671cd2815ab21e
katiewoodard <woodard.katelyn@gmail.com>
2016-06-10 16:21:25 -0700

    Added plot definition to tweetViz and resulting image as test.png. Will want to change the data set to be more than just English to have more interesting graph.

--- katie/tweetViz.py
+++ katie/tweetViz.py
@@ -1,6 +1,6 @@
 import json
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 tweets_data = []
@@ -17,4 +17,17 @@ print len(tweets_data)
 tweets = pd.DataFrame()
 tweets['text'] = map(lambda tweet: tweet['text'], tweets_data)
 tweets['lang'] = map(lambda tweet: tweet['lang'], tweets_data)
-tweets['country'] = map(lambda tweet: tweet['place']['country'] if tweet[place] != None else None, tweets_data)
+tweets['country'] = map(lambda tweet: tweet['place']['country'] if tweet['place'] != None else None, tweets_data)
+
+tweets_by_lang = tweets['lang'].value_counts()
+#print tweets_by_lang
+
+#below creates a bar chart using matplotlib (matlab) plt.savefig saves it to a png in the current folder
+fig, ax = plt.subplots()
+ax.tick_params(axis = 'x', labelsize=15)
+ax.tick_params(axis = 'y', labelsize=10)
+ax.set_xlabel('Languages', fontsize = 15)
+ax.set_ylabel('Number of Tweets', fontsize = 15)
+ax.set_title('Top 5 languages', fontsize = 15, fontweight = 'bold')
+tweets_by_lang[:5].plot(ax = ax, kind = 'bar', color='blue')
+plt.savefig('katie.png')
