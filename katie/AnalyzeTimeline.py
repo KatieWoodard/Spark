ff2ba3a392dddda9552bee657279a7fafbdb94c5
katiewoodard <woodard.katelyn@gmail.com>
2016-06-20 13:26:58 -0700

    cleaning up comments in AnalyzeTimeline.py

--- katie/AnalyzeTimeline.py
+++ katie/AnalyzeTimeline.py
@@ -5,6 +5,8 @@ from wordcloud import WordCloud
 import pandas as pd
 import re
 import json
+from nltk.tokenize import word_tokenize
+
 
 all_data = []
 with open('TMobile.json', 'r') as f:
@@ -12,6 +14,48 @@ with open('TMobile.json', 'r') as f:
         tweet = json.loads(line) # load it as Python dict
         only_text = json.dumps(tweet['text'])
         all_data.append(only_text)
+
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>', # HTML tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+    r'(?:[\w_]+)', # other words
+    r'(?:\S)' # anything else
+]
+
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+
+def tokenize(s):
+    return tokens_re.findall(s)
+
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+stopwords = ['what','who','is','a','at','is','the', 'amp', 'https', 'rt', 'u2026', 'ud83d', '@tmobile', '@johnlegere', '.co', 't.co', 'co.']
+all_data = []
+with open('TMobile.json', 'r') as f:
+    for line in f:
+        tweet = json.loads(line) # load it as Python dict
+        only_text = preprocess(tweet['text'])
+        joined_text = ' '.join(word for word in only_text if word.lower() not in stopwords and 'https' not in word)
+        print joined_text
+        all_data.append(joined_text)
 
 #print all_data
 
@@ -21,21 +65,6 @@ wordcloud = WordCloud().generate(data)
 plt.imshow(wordcloud)
 plt.axis("off")
 plt.savefig('TmobileWordCloud.png')
-
-query = data
-stopwords = ['what','who','is','a','at','is','the', 'amp', 'https', 'rt', 'u2026', 'ud83d', '@tmobile', '@johnlegere']
-querywords = query.split()
-resultwords  = [word for word in querywords if word.lower() not in stopwords]
-print resultwords
-result = ' '.join(resultwords)
-print result
-#result = ' '.join(filter(lambda x: x.lower() not in stopwords, data.split()))
-
-wordcloud2 = WordCloud().generate(result)
-plt.imshow(wordcloud2)
-plt.axis("off")
-plt.savefig('2testTmobileWordCloudTryStopWords.png')
-
 
 #sc = SparkContext("local", "Pysparktest")
 #sql = SQLContext(sc)
@@ -48,41 +77,4 @@ df_distinctlang = df.select("lang").distinct()
 df_lang = sql.read.json("TwitterLanguages.json")
 df_join =df_distinctlang.join(df_lang, df_distinctlang.lang == df_lang.code).drop(df_lang.code).drop(df_lang.status)
 df_join.show()
-"""
-#try pandas dataframe
-"""
-
-tweets_data = []
-tweets_file = open('HitachiTimeline.json','r')
-for line in tweets_file:
-    try:
-        tweet = json.loads(line)
-        #need to check that the line is actually a tweet, twitter also sends limit info as lines through getTweets
-        tweets_data.append(tweet)
-    except:
-        continue
-
-print len(tweets_data)
-
-tweets = pd.DataFrame()
-tweets["text"] = map(lambda tweet: tweet["text"], tweets_data)
-#tweets["lang"] = map(lambda tweet: tweet["lang"], tweets_data)
-
-total_text = ''
-for row in tweets.iterrows():
-    total_text = total_text + ' ' + row
-
-print total_text
-"""
-"""
-df_text = df.select("text")
-df_1 =  df_text.take(1)
-print str(df_1)
-"""
-
-"""
-wordcloud = WordCloud().generate(df_1)
-plt.imshow(wordcloud)
-plt.axis("off")
-plt.savefig('test.png')
 """
